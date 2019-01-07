f5647701a4dcb0e5a911fd821b759c7b22df11a2
Tom Hartwell <thartwell@awarepoint.com>
2019-01-06 22:06:21 -0800

    Updates

---
+++ precache-manifest.7f7459c0bcf38b3f71c147bbe34b16e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ac6ad11f95723b6ebc7",
+    "url": "/reg-viewer/static/js/runtime~main.3ac6ad11.js"
+  },
+  {
+    "revision": "b1939cb50e14c7acbca5",
+    "url": "/reg-viewer/static/js/main.b1939cb5.chunk.js"
+  },
+  {
+    "revision": "e604bbaef618e55aa0c4",
+    "url": "/reg-viewer/static/js/1.e604bbae.chunk.js"
+  },
+  {
+    "revision": "b1939cb50e14c7acbca5",
+    "url": "/reg-viewer/static/css/main.7d79dbce.chunk.css"
+  },
+  {
+    "revision": "e604bbaef618e55aa0c4",
+    "url": "/reg-viewer/static/css/1.660d644e.chunk.css"
+  },
+  {
+    "revision": "fb744737aa50e9c99c2b3b3feb84ff01",
+    "url": "/reg-viewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a04701d2fca0e95496e56b0323d48ec.js"
+  "/reg-viewer/precache-manifest.7f7459c0bcf38b3f71c147bbe34b16e0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reg-viewer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
